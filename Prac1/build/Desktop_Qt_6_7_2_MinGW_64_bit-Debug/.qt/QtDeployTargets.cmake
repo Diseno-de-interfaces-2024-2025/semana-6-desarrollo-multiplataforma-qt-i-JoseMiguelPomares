edcf99240f2a2a6c099b5d2d9355b30ebecce46c
JoseMiguelPomares <josedeberes2709@gmail.com>
2024-10-31 17:42:44 +0100

    1

---
+++ Prac1/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_appPrac1_FILE C:/Users/jospompam/Desktop/Desarrollo-de-Interfaces/semana-6-desarrollo-multiplataforma-qt-i-JoseMiguelPomares/Prac1/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/appPrac1.exe)
+set(__QT_DEPLOY_TARGET_appPrac1_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_appPrac1_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Quick.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6QmlModels.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Qml.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Network.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
